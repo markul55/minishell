98c15ca34412f10b8cc8e89ea21d1a258131aba1
Franciszek Markul <fmarkul@c4r1s1.42warsaw.pl>
2025-08-01 17:47:34 +0200

    main nie dziala

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
-#include "libft.h"
+#include "./libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 int main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		if(av[1] == "echo")
-		{
-		p
-		}
+	char *input;
+
+    input = readline("Wpisz coś: ");
+
+	if (input = "echo")
+	{
+		printf("%s\n", av[1]);
 	}
+	}
+	printf("\n");
+	return(0);
 }
